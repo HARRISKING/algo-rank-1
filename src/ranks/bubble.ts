75e5b1dc0e93b1f6dbb082dd26a7215c26458d31
suixing <suixing@uni-ubi.com>
2022-04-19 20:20:28 +0800

    feat: eslint优化

--- src/ranks/bubble.ts
+++ src/ranks/bubble.ts
@@ -1,9 +1,9 @@
 // 源数据
 const dataSource: number[] = [4, 7, 29, 1, 3, 23, 25, 26, 43, 55, 2];
 // 冒泡算法
-const bubble = (data) => {
+const bubble = (data: number[]) => {
   // 防止污染源数据，进行深拷贝
-  let copyArr = JSON.parse(JSON.stringify(data));
+  let copyArr: number[] = JSON.parse(JSON.stringify(data));
   for (let i = 0; i < copyArr.length; i++) {
     for (let j = 0; j < copyArr.length - i; j++) {
       const leftItem: number = copyArr[j],
@@ -15,7 +15,7 @@ const bubble = (data) => {
   }
 };
 // 换位置处理函数
-const switchPosition = (index: number, targetList: number[]) => {
+const switchPosition = (index: number, targetList: number[]): number[] => {
   const copyArr = JSON.parse(JSON.stringify(targetList));
   const temp = copyArr[index];
   copyArr[index] = copyArr[index + 1];
